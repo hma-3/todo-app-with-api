f2aa88b1b778cc95cd0e7a6a2549eaf88b378c39
Mariia <maria.gula0307@gmail.com>
2024-11-20 14:01:34 +0200

    cloned todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,79 @@
-import React from 'react';
-import './App.scss';
+import { useTodo } from './hooks/useTodo';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+import { TodoHeader } from './components/TodoHeader';
+import { TodoList } from './components/TodoList';
+import { TodoFooter } from './components/TodoFooter';
+import { TodoErrorNotification } from './components/TodoErrorNotification';
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+export const App = () => {
+  const {
+    todos,
+    todosAmount,
+    loadingTodoIds,
+    tempTodo,
+    editingTodo,
+    setEditingTodo,
+    activeTodosAmount,
+    errorMessage,
+    handleResetErrorMessage,
+    statusFilter,
+    setStatusFilter,
+    handleDeleteTodo,
+    handleClearCompleted,
+    isFocusedNewTodoInput,
+    newTodoTitle,
+    handleNewTodoTitleChange,
+    isLoadingNewTodoSubmit,
+    handleAddTodoFormSubmit,
+    handleToggleTodo,
+    handleToggleAllTodos,
+    handleRenameTodo,
+  } = useTodo();
 
-export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <TodoHeader
+          todosAmount={todosAmount}
+          activeTodosAmount={activeTodosAmount}
+          isFocusedInput={isFocusedNewTodoInput}
+          newTodoTitle={newTodoTitle}
+          onTitleChange={handleNewTodoTitleChange}
+          isLoadingSubmit={isLoadingNewTodoSubmit}
+          onSubmitForm={handleAddTodoFormSubmit}
+          onToggleAllTodos={handleToggleAllTodos}
+        />
+
+        <TodoList
+          todos={todos}
+          todosAmount={todosAmount}
+          loadingTodoIds={loadingTodoIds}
+          tempTodo={tempTodo}
+          editingTodo={editingTodo}
+          setEditingTodo={setEditingTodo}
+          statusFilter={statusFilter}
+          onDeleteTodo={handleDeleteTodo}
+          onToggleTodo={handleToggleTodo}
+          onRenameTodo={handleRenameTodo}
+        />
+
+        {!!todosAmount && (
+          <TodoFooter
+            leftTodos={activeTodosAmount}
+            statusFilter={statusFilter}
+            onChangeStatusFilter={setStatusFilter}
+            todosAmount={todosAmount}
+            onClearCompleted={handleClearCompleted}
+          />
+        )}
+      </div>
+
+      <TodoErrorNotification
+        errorMessage={errorMessage}
+        onResetErrorMessage={handleResetErrorMessage}
+      />
     </div>
   );
 };
